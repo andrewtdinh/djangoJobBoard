813e0b1b992fa2040967cad247b5b485fd78bf70
Andrew Dinh <andrewdinh74@yahoo.com>
2025-10-15 11:47:14 -0700

    update model with is_active field

--- job_board/models.py
+++ job_board/models.py
@@ -6,6 +6,8 @@ class JobPosting(models.Model):
   description = models.TextField()
   company = models.CharField(max_length=100)
   salary = models.IntegerField()
+  is_active = models.BooleanField(default=False)
+  
 
 # CRUD Operations
 
