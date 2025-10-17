8568fa628e17503d615903cd3286ec6d544ffc4a
Andrew Dinh <andrewdinh74@yahoo.com>
2025-10-16 17:55:52 -0700

    Bring in humanize template filter to numbers look nice

--- job_board/views.py
+++ job_board/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, HttpResponse
+from .models import JobPosting
 
 # Create your views here.
 def index(request):
-  return HttpResponse('<h1>Job Board</h1>')
+  active_postings = JobPosting.objects.filter(is_active=True)
+  context = {
+    'job_postings': active_postings
+  }
+  return render(request, 'job_board/index.html', context)
